b451231f45e1d1a91d2effe4c298a05c9fa4aac6
aeuna <4294psy@naver.com>
2021-07-27 23:50:16 +0900

    antd 사용해 SNS화면 만들기 - 로그인폼

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,21 +1,60 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
+import styled from "styled-components";
+import { Menu, Input, Row, Col } from "antd";
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-      <div>
-        <Link href="/">
-          <a>노드버드</a>
-        </Link>
-        <Link href="/profile">
-          <a>프로필</a>
-        </Link>
-        <Link href="/signup">
-          <a>회원가입</a>
-        </Link>
-      </div>
-      {children}
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <SearchInput enterButton />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? (
+            <UserProfile setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+            <LoginForm setIsLoggedIn={setIsLoggedIn} />
+          )}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://pale-freighter-43a.notion.site/Euna-s-Portfolio-222e1df5690b49628d2b87a60b3f7476"
+            target="_blank"
+            rel="noreferrer noopenner"
+          >
+            Made by euna
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
