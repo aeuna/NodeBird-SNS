0cfeee4d20b0a827650a348159e58ccd26c11e51
aeuna <4294psy@naver.com>
2021-08-08 08:19:21 +0900

    antd 사용해 SNS화면 만들기 - 프로필

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,14 +1,30 @@
 import AppLayout from "../components/AppLayout";
 import Head from "next/head";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
+  const followingList = [
+    { nickname: "euna" },
+    { nickname: "aeuan" },
+    { nickname: "beuna" },
+  ];
+  const followerList = [
+    { nickname: "euna" },
+    { nickname: "aeuan" },
+    { nickname: "beuna" },
+  ];
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <AppLayout>프로필화면</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList heder="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
