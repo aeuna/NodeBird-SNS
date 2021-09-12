f053f4865f337bb633f0e0bd23c14b56de97f853
aeuna <4294psy@naver.com>
2021-09-12 19:41:49 +0900

    Redux연동하기 - 리덕스구현하기

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import styled from "styled-components";
+import { useSelector } from "react-redux";
 import { Menu, Input, Row, Col } from "antd";
 import UserProfile from "./UserProfile";
 import LoginForm from "./LoginForm";
@@ -11,7 +12,8 @@ const SearchInput = styled(Input.Search)`
 `;
 
 const AppLayout = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
   return (
     <div>
       <Menu mode="horizontal">
@@ -36,11 +38,7 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          {isLoggedIn ? (
-            <UserProfile setIsLoggedIn={setIsLoggedIn} />
-          ) : (
-            <LoginForm setIsLoggedIn={setIsLoggedIn} />
-          )}
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,8 +1,9 @@
 import { createWrapper } from "next-redux-wrapper";
 import { createStore } from "redux";
+import reducer from "../reducers";
 
 const configureStore = () => {
-  const store = createStore();
+  const store = createStore(reducer);
   return store;
 };
 
